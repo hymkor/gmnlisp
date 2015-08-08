fcb581d60fdefcb7df94f4b0c01c83816772e691
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-08-08 15:11:12 +0900

    Print error on test.

--- eval_test.go
+++ eval_test.go
@@ -2,11 +2,15 @@ package gommon
 
 import (
 	"fmt"
+	"os"
 	"testing"
 )
 
 func TestEval(t *testing.T) {
 	fmt.Println("Start TestEval")
-	ParseString("(print 1 2 3 4 5)").Eval()
+	_,err := ParseString("(print 1 (quote 2 3) 4 5)").Eval()
+	if err != nil {
+		fmt.Fprint(os.Stderr,err.Error())
+	}
 	fmt.Println("End TestEval")
 }
