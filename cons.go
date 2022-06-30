23a89f583b6b08d5ff1e0872c83a119a31505873
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-07-01 08:27:25 +0900

    cons: draw as simple style (X . Y) only

--- cons.go
+++ cons.go
@@ -1,7 +1,6 @@
 package gommon
 
 import (
-	"fmt"
 	"io"
 )
 
@@ -13,41 +12,39 @@ type Cons struct {
 func (this *Cons) Null() bool {
 	return false
 }
+func IsNull(node Node) bool {
+	return node == nil || node.Null()
+}
+
+func write(n *int64, w io.Writer, s string) error {
+	_n, err := io.WriteString(w, s)
+	*n += int64(_n)
+	return err
+}
 
 func (this *Cons) WriteTo(w io.Writer) (int64, error) {
 	var n int64
-	m, err := fmt.Fprint(w, "( ")
-	n += int64(m)
+	if err := write(&n, w, "("); err != nil {
+		return n, err
+	}
+
+	m, err := this.Car.WriteTo(w)
+	n += m
 	if err != nil {
 		return n, err
 	}
 
-	for this != nil && !this.Null() {
-		m, err := this.Car.WriteTo(w)
+	if !IsNull(this.Cdr) {
+		// output as ( X . Y )
+		if err := write(&n, w, " . "); err != nil {
+			return n, err
+		}
+		m, err := this.Cdr.WriteTo(w)
 		n += m
 		if err != nil {
 			return n, err
 		}
-		p, ok := this.Cdr.(*Cons)
-		if !ok {
-			if this.Cdr == nil || this.Cdr.Null() {
-				break
-			}
-			_m, err := fmt.Fprint(w, " . ")
-			n += int64(_m)
-			if err != nil {
-				return n, err
-			}
-			m, err := this.Cdr.WriteTo(w)
-			n += m
-			if err != nil {
-				return n, err
-			}
-		}
-		this = p
-		fmt.Fprint(w, " ")
 	}
-	m, err = fmt.Fprint(w, " )")
-	n += int64(m)
+	err = write(&n, w, ")")
 	return n, nil
 }
