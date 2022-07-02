90fa68122028e234302e11d37679dae9927dda92
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-07-02 13:44:23 +0900

    Add: example.go

---
+++ example.go
@@ -0,0 +1,30 @@
+//go:build run
+// +build run
+
+package main
+
+import (
+	"fmt"
+	"github.com/hymkor/gommon"
+	"os"
+)
+
+func mains(args []string) error {
+	for _, code := range args {
+		fmt.Printf("   %s\n-> ", code)
+		result, err := gommon.ReadString(code).Eval()
+		if err != nil {
+			return err
+		}
+		result.WriteTo(os.Stdout)
+		fmt.Println()
+	}
+	return nil
+}
+
+func main() {
+	if err := mains(os.Args[1:]); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
