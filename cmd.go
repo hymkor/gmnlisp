160fc0396db9e8377fb582cce51a91135caa5178
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-07-02 10:40:00 +0900

    Refine (print)

--- cmd.go
+++ cmd.go
@@ -5,39 +5,6 @@ import (
 	"os"
 )
 
-func CmdPrint(this Node) (Node, error) {
-	if cons, ok := this.(*Cons); ok {
-		delim := ""
-		for cons != nil && !cons.Null() {
-			fmt.Print(delim)
-			delim = " "
-			if cons1, ok := cons.Car.(*Cons); ok {
-				val, err := cons1.Eval()
-				if err != nil {
-					return nil, err
-				}
-				val.WriteTo(os.Stdout)
-			} else if cons.Car != nil {
-				cons.Car.WriteTo(os.Stdout)
-			} else {
-				fmt.Fprint(os.Stdout, "<nil>")
-			}
-			if cons1, ok := cons.Cdr.(*Cons); ok {
-				cons = cons1
-			} else if cons.Cdr == nil || cons.Cdr.Null() {
-				break
-			} else {
-				cons.Cdr.WriteTo(os.Stdout)
-				break
-			}
-		}
-	} else if this != nil && !this.Null() {
-		this.WriteTo(os.Stdout)
-	}
-	fmt.Println()
-	return nil, nil
-}
-
 func CmdQuote(this Node) (Node, error) {
 	return this, nil
 }
@@ -60,6 +27,18 @@ func ForEachList(this Node, f func(Node) error) error {
 		}
 		this = cons.Cdr
 	}
+}
+
+func CmdPrint(this Node) (Node, error) {
+	dem := ""
+	err := ForEachList(this, func(one Node) error {
+		fmt.Print(dem)
+		_, err1 := one.WriteTo(os.Stdout)
+		dem = " "
+		return err1
+	})
+	fmt.Println()
+	return &Null{}, err
 }
 
 func CmdPlus(this Node) (Node, error) {
