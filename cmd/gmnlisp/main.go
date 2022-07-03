b5ddab344d3171f1b1ad471207a4b5798b9b7f2c
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-07-04 08:58:49 +0900

    Add "cmd/gmnlisp" as prototype (only option -e)

---
+++ cmd/gmnlisp/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/hymkor/gommon"
+)
+
+var flagExecute = flag.String("e", "", "execute string")
+
+func mains(args []string) error {
+	if *flagExecute != "" {
+		result, err := gommon.Interpret(*flagExecute)
+		if err != nil {
+			return err
+		}
+		result.WriteTo(os.Stdout)
+		fmt.Println()
+	}
+	return nil
+}
+
+func main() {
+	flag.Parse()
+	if err := mains(flag.Args()); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
