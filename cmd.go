03010d034a892fffc394a420d2cfab6d35804eaa
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-07-06 11:13:19 +0900

    Make error objects (ErrTooFewOrTooManyArguments...)

--- cmd.go
+++ cmd.go
@@ -7,11 +7,17 @@ import (
 	"os"
 )
 
+var (
+	ErrTooFewOrTooManyArguments = errors.New("Too few or too many arguments")
+	ErrExpectedCons             = errors.New("Expected CONS")
+	ErrExpectedNumber           = errors.New("Expected a number")
+)
+
 func ForEachQuote(this Node, f func(Node) error) error {
 	for {
 		cons, ok := this.(*Cons)
 		if !ok {
-			return fmt.Errorf("Not a list: %s", Node2String(this))
+			return fmt.Errorf("%w (%s)", ErrExpectedCons, Node2String(this))
 		}
 		if err := f(cons.Car); err != nil {
 			return err
@@ -50,7 +56,7 @@ func CmdPlus(this Node) (Node, error) {
 	err := ForEachEval(this, func(one Node) error {
 		value, ok := one.(NodeInteger)
 		if !ok {
-			return fmt.Errorf("Not A Number: %s", Node2String(one))
+			return fmt.Errorf("+: %w %s", ErrExpectedNumber, Node2String(one))
 		}
 		result += value
 		return nil
@@ -63,7 +69,7 @@ func CmdCons(node Node) (Node, error) {
 	i := 0
 	err := ForEachEval(node, func(n Node) error {
 		if i >= len(result) {
-			return errors.New("cons: parameter number not 2")
+			return fmt.Errorf("cons: %w", ErrTooFewOrTooManyArguments)
 		}
 		result[i] = n
 		i++
@@ -75,16 +81,16 @@ func CmdCons(node Node) (Node, error) {
 func CmdCar(this Node) (Node, error) {
 	cons, ok := this.(*Cons)
 	if !ok {
-		return nil, fmt.Errorf("Not a list: %s", Node2String(this))
+		return nil, fmt.Errorf("car: %w", ErrTooFewOrTooManyArguments)
 	}
 
 	firstArg, err := cons.Car.Eval()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("car: %w", err)
 	}
 	cons, ok = firstArg.(*Cons)
 	if !ok {
-		return nil, fmt.Errorf("Not a list: %s", Node2String(firstArg))
+		return nil, fmt.Errorf("car: %w", ErrExpectedCons)
 	}
 	return cons.Car, nil
 }
@@ -94,7 +100,7 @@ func CmdCdr(param Node) (Node, error) {
 	err := ForEachEval(param, func(firstArg Node) error {
 		cons, ok := firstArg.(*Cons)
 		if !ok {
-			return fmt.Errorf("Not a list: %s", Node2String(firstArg))
+			return ErrExpectedCons
 		}
 		result = cons.Cdr
 		return io.EOF
@@ -103,16 +109,16 @@ func CmdCdr(param Node) (Node, error) {
 	case io.EOF:
 		return result, nil
 	case nil:
-		return nil, errors.New("Too few parameters")
+		return nil, fmt.Errorf("cdr: %w", ErrTooFewOrTooManyArguments)
 	default:
-		return nil, err
+		return nil, fmt.Errorf("cdr: %w", err)
 	}
 }
 
 func CmdQuote(param Node) (Node, error) {
 	cons, ok := param.(*Cons)
 	if !ok {
-		return nil, fmt.Errorf("Not a list: %s", Node2String(param))
+		return nil, fmt.Errorf("quote: %w", ErrTooFewOrTooManyArguments)
 	}
 	return cons.Car, nil
 }
@@ -122,7 +128,7 @@ var T = NodeSymbol("T")
 func CmdAtom(param Node) (Node, error) {
 	cons, ok := param.(*Cons)
 	if !ok {
-		return nil, fmt.Errorf("Not a list: %s", Node2String(param))
+		return nil, fmt.Errorf("atom: %w", ErrExpectedCons)
 	}
 	if _, ok := cons.Car.(*Cons); ok {
 		return &Null{}, nil
@@ -150,5 +156,5 @@ func CmdEq(param Node) (Node, error) {
 	if err == nil {
 		return T, nil
 	}
-	return &Null{}, err
+	return &Null{}, fmt.Errorf("eq: %w", err)
 }
