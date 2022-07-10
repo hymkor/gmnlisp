ffa4274087e5907fa68f679fdf295b347012d1e6
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-07-10 22:48:29 +0900

    ( Reduce reduntant messages from tokenize test )

--- tokenize_test.go
+++ tokenize_test.go
@@ -7,32 +7,29 @@ import (
 func TestStringToToken(t *testing.T) {
 	x := StringToTokens("a b c")
 	if len(x) != 3 {
-		t.Error("x-len\n")
+		t.Fatal("x-len\n")
 	}
 	if x[0] != "a" || x[1] != "b" || x[2] != "c" {
-		t.FailNow()
+		t.Fatal("`a b c` not `a` `b` `c`")
 	}
 
 	y := StringToTokens("(a b c)")
-	for i := 0; i < len(y); i++ {
-		t.Logf("y[%d]=='%s'\n", i, y[i])
-	}
 	if len(y) != 5 {
-		t.Errorf("y-len == %d\n", len(y))
+		t.Fatalf("y-len == %d\n", len(y))
 	}
 	if y[0] != "(" {
-		t.Error("y[0]\n")
+		t.Fatal("y[0]")
 	}
 	if y[1] != "a" {
-		t.Error("y[1]\n")
+		t.Fatal("y[1]")
 	}
 	if y[2] != "b" {
-		t.Error("y[2]\n")
+		t.Fatal("y[2]")
 	}
 	if y[3] != "c" {
-		t.Error("y[3]\n")
+		t.Fatal("y[3]")
 	}
 	if y[4] != ")" {
-		t.Error("y[4]\n")
+		t.Fatal("y[4]")
 	}
 }
