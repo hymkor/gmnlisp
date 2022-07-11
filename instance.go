beae279273cc54374e2391e657cc083cfda54274
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-07-11 09:02:53 +0900

    Move code for (*Instance)Interpret to "instance.go"

--- instance.go
+++ instance.go
@@ -91,3 +91,22 @@ func forEachWithoutEval(this Node, f func(Node) error) error {
 	}
 	return nil
 }
+
+func (ins *Instance) Interpret(code string) (Node, error) {
+	compiled, err := ReadString(code)
+	if err != nil {
+		return nil, err
+	}
+	var result Node
+	for _, c := range compiled {
+		c, err = macroQuote(c)
+		if err != nil {
+			return c, err
+		}
+		result, err = c.Eval(ins)
+		if err != nil {
+			return result, err
+		}
+	}
+	return result, err
+}
