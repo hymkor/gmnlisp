716e5b1c95e6398fa87eca53c2d7b0d4185451ac
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-07-19 00:54:30 +0900

    Add testcode for (macroexpand) which can not succeed now yet

--- macro_test.go
+++ macro_test.go
@@ -17,3 +17,13 @@ func TestMacro(t *testing.T) {
 			(dbl (incf a1)))
 	`, Integer(7))
 }
+
+func TestMacroExpand(t *testing.T){
+	assertEqual(t,`
+		(defmacro dbl  (x) (list '+ x x))
+		(defmacro incf (x) (setq x (+ x 1)))
+		(let ((a1 2))
+			(macroexpand '(dbl (incf a1))))
+	`, List(Symbol("+"),List(Symbol("incf"),Symbol("a1")),
+		List(Symbol("incf"),Symbol("a1"))))
+}
