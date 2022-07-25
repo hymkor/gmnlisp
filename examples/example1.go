3b5f01b6511a7cb3bdd7646f4d62b8fdb0427213
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-07-25 21:23:35 +0900

    example1.go: append Context parameter

--- examples/example1.go
+++ examples/example1.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
@@ -14,7 +15,7 @@ func main() {
 	lisp := gmnlisp.New()
 	lisp.Set("a", gmnlisp.Integer(1))
 	lisp.Set("b", gmnlisp.Integer(2))
-	value, err := lisp.Interpret("(+ a b)")
+	value, err := lisp.Interpret(context.TODO(),"(+ a b)")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		return
