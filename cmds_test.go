7f0b7bcea8b26b01cd5a42b769d9e6dabf0cff50
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-07-30 15:49:45 +0900

    Divide the code in "cmds_test.go"

--- cmds_test.go
+++ cmds_test.go
@@ -8,13 +8,17 @@ func TestCmdGo(t *testing.T) {
 	assertEqual(t, "(quote (1 . 2))", &Cons{Car: Integer(1), Cdr: Integer(2)})
 	assertEqual(t, "(atom 1)", True)
 	assertEqual(t, "(atom '(1 2))", Null)
-
 	assertEqual(t, `(equal (list 1 (+ 1 1) (+ 1 2)) '(1 2 3))`, True)
+}
+
+func TestNotEqual(t *testing.T) {
 	assertEqual(t, `(not (/= 1 1))`, True)
 	assertEqual(t, `(not (= 1 1))`, Null)
 	assertEqual(t, `(/= 1 2)`, True)
 	assertEqual(t, `(/= 1 1)`, Null)
+}
 
+func TestZerop(t *testing.T) {
 	assertEqual(t, `(zerop 0)`, True)
 	assertEqual(t, `(zerop 0.0)`, True)
 	assertEqual(t, `(zerop 1)`, Null)
@@ -22,7 +26,7 @@ func TestCmdGo(t *testing.T) {
 	assertEqual(t, `(zerop "")`, Null)
 }
 
-func TestNumberp(t *testing.T){
+func TestNumberp(t *testing.T) {
 	assertEqual(t, `(numberp 1)`, True)
 	assertEqual(t, `(numberp 1.0)`, True)
 	assertEqual(t, `(numberp "")`, Null)
