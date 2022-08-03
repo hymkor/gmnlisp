a61d042ea0cc490b5da0d7e8d6f220fbc2b272c0
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-08-04 01:19:42 +0900

    Improve with _SequenceBuilder

--- sequence.go
+++ sequence.go
@@ -2,7 +2,6 @@ package gmnlisp
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"math"
 	"strings"
@@ -10,6 +9,87 @@ import (
 
 type _Sequence interface {
 	firstAndRest() (Node, Node, bool)
+}
+
+func seqEach(list Node, f func(Node) error) error {
+	for HasValue(list) {
+		seq, ok := list.(_Sequence)
+		if !ok {
+			return ErrExpectedSequence
+		}
+		var value Node
+
+		value, list, ok = seq.firstAndRest()
+		if !ok {
+			break
+		}
+		if err := f(value); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type _SeqBuilder interface {
+	Add(Node) error
+	Sequence() Node
+}
+
+type _ListBuilder struct {
+	first Cons
+	last  *Cons
+}
+
+func (L *_ListBuilder) Add(n Node) error {
+	tmp := &Cons{
+		Car: n,
+		Cdr: Null,
+	}
+	if L.last == nil {
+		L.first.Cdr = tmp
+	} else {
+		L.last.Cdr = tmp
+	}
+	L.last = tmp
+	return nil
+}
+
+func (L *_ListBuilder) Sequence() Node {
+	return L.first.Cdr
+}
+
+type _StringBuilder struct {
+	buffer strings.Builder
+}
+
+func (S *_StringBuilder) Add(n Node) error {
+	r, ok := n.(Rune)
+	if !ok {
+		return ErrExpectedCharacter
+	}
+	S.buffer.WriteRune(rune(r))
+	return nil
+}
+
+func (S *_StringBuilder) Sequence() Node {
+	return String(S.buffer.String())
+}
+
+var sequenceBuilderTable = map[Symbol](func() _SeqBuilder){
+	symbolForList:   func() _SeqBuilder { return &_ListBuilder{} },
+	symbolForString: func() _SeqBuilder { return &_StringBuilder{} },
+}
+
+func newSeqBuilder(symbolNode Node) (_SeqBuilder, error) {
+	symbol, ok := symbolNode.(Symbol)
+	if !ok {
+		return nil, ErrExpectedSymbol
+	}
+	builder, ok := sequenceBuilderTable[symbol]
+	if !ok {
+		return nil, ErrNotSupportType
+	}
+	return builder(), nil
 }
 
 const (
@@ -56,47 +136,6 @@ func funTypep(_ context.Context, _ *World, args []Node) (Node, error) {
 	return Null, nil
 }
 
-func coerceToList(list Node) (Node, error) {
-	return list, nil
-}
-
-func coerceToString(list Node) (Node, error) {
-	var buffer strings.Builder
-	err := seqEach(list, func(value Node) error {
-		r, ok := value.(Rune)
-		if !ok {
-			return fmt.Errorf("%w: `%s`", ErrExpectedCharacter, toString(value, PRINT))
-		}
-		buffer.WriteRune(rune(r))
-		return nil
-	})
-	return String(buffer.String()), err
-}
-
-var coerceTable = map[Symbol]func(list Node) (Node, error){
-	symbolForList:   coerceToList,
-	symbolForString: coerceToString,
-}
-
-func seqEach(list Node, f func(Node) error) error {
-	for HasValue(list) {
-		seq, ok := list.(_Sequence)
-		if !ok {
-			return ErrExpectedSequence
-		}
-		var value Node
-
-		value, list, ok = seq.firstAndRest()
-		if !ok {
-			break
-		}
-		if err := f(value); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 func funAref(_ context.Context, _ *World, args []Node) (Node, error) {
 	index, ok := args[1].(Integer)
 	if !ok {
@@ -115,63 +154,14 @@ func funAref(_ context.Context, _ *World, args []Node) (Node, error) {
 	return value, nil
 }
 
-type _SequenceBuilder interface {
-	Add(Node) error
-	Sequence() Node
-}
-
-type _StringBuilder struct {
-	buffer strings.Builder
-}
-
-func (S *_StringBuilder) Add(n Node) error {
-	r, ok := n.(Rune)
-	if !ok {
-		return ErrExpectedCharacter
-	}
-	S.buffer.WriteRune(rune(r))
-	return nil
-}
-
-func (S *_StringBuilder) Sequence() Node {
-	return String(S.buffer.String())
-}
-
-type _ListBuilder struct {
-	first Cons
-	last  *Cons
-}
-
-func (L *_ListBuilder) Add(n Node) error {
-	tmp := &Cons{
-		Car: n,
-		Cdr: Null,
-	}
-	if L.last == nil {
-		L.first.Cdr = tmp
-	} else {
-		L.last.Cdr = tmp
-	}
-	L.last = tmp
-	return nil
-}
-
-func (L *_ListBuilder) Sequence() Node {
-	return L.first.Cdr
-}
-
 func funConcatenate(ctx context.Context, w *World, list []Node) (Node, error) {
 	if len(list) < 1 {
 		return Null, nil
 	}
-
-	symbol, ok := list[0].(Symbol)
-	if !ok {
-		return nil, ErrExpectedSymbol
+	buffer, err := newSeqBuilder(list[0])
+	if err != nil {
+		return nil, err
 	}
-
-	var buffer _ListBuilder
-
 	for _, element := range list[1:] {
 		err := seqEach(element, func(value Node) error {
 			buffer.Add(value)
@@ -181,11 +171,7 @@ func funConcatenate(ctx context.Context, w *World, list []Node) (Node, error) {
 			return nil, err
 		}
 	}
-	collector, ok := coerceTable[symbol]
-	if !ok {
-		return nil, ErrNotSupportType
-	}
-	return collector(buffer.Sequence())
+	return buffer.Sequence(), nil
 }
 
 func funLength(_ context.Context, _ *World, argv []Node) (Node, error) {
@@ -197,34 +183,33 @@ func funLength(_ context.Context, _ *World, argv []Node) (Node, error) {
 	return Integer(length), err
 }
 
-func mapCar(ctx context.Context, w *World, funcNode Node, listSet []Node) (result Node, err error) {
+func mapCar(ctx context.Context, w *World, funcNode Node, listSet []Node, resultSet _SeqBuilder) error {
 	f, err := funcNode.Eval(ctx, w)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	_f, ok := f.(_Callable)
 	if !ok {
-		return nil, ErrExpectedFunction
+		return ErrExpectedFunction
 	}
-	var resultSet _ListBuilder
 	for {
 		paramSet := make([]Node, len(listSet))
 		for i := 0; i < len(listSet); i++ {
 			if IsNull(listSet[i]) {
-				return resultSet.Sequence(), nil
+				return nil
 			}
 			seq, ok := listSet[i].(_Sequence)
 			if !ok {
-				return nil, ErrNotSupportType
+				return ErrNotSupportType
 			}
 			paramSet[i], listSet[i], ok = seq.firstAndRest()
 			if !ok {
-				return resultSet.Sequence(), nil
+				return nil
 			}
 		}
 		result, err := _f.Call(ctx, w, List(paramSet...))
 		if err != nil {
-			return nil, err
+			return err
 		}
 		resultSet.Add(result)
 	}
@@ -234,38 +219,33 @@ func funMapCar(ctx context.Context, w *World, argv []Node) (Node, error) {
 	if len(argv) < 1 {
 		return nil, ErrTooFewArguments
 	}
-	return mapCar(ctx, w, argv[0], argv[1:])
+	var buffer _ListBuilder
+	err := mapCar(ctx, w, argv[0], argv[1:], &buffer)
+	return buffer.Sequence(), err
 }
 
 func funMap(ctx context.Context, w *World, argv []Node) (Node, error) {
 	if len(argv) < 2 {
 		return nil, ErrTooFewArguments
 	}
-	symbol, ok := argv[0].(Symbol)
-	if !ok {
-		return nil, ErrExpectedSymbol
-	}
-	collector, ok := coerceTable[symbol]
-	if !ok {
-		return nil, ErrNotSupportType
-	}
-	result, err := mapCar(ctx, w, argv[1], argv[2:])
+	buffer, err := newSeqBuilder(argv[0])
 	if err != nil {
 		return nil, err
 	}
-	return collector(result)
+	err = mapCar(ctx, w, argv[1], argv[2:], buffer)
+	return buffer.Sequence(), err
 }
 
 func funCoerce(_ context.Context, _ *World, argv []Node) (Node, error) {
-	symbol, ok := argv[1].(Symbol)
-	if !ok {
-		return nil, ErrExpectedSymbol
-	}
-	collector, ok := coerceTable[symbol]
-	if !ok {
+	buffer, err := newSeqBuilder(argv[1])
+	if err != nil {
 		return nil, ErrNotSupportType
 	}
-	return collector(argv[0])
+	err = seqEach(argv[0], func(value Node) error {
+		buffer.Add(value)
+		return nil
+	})
+	return buffer.Sequence(), err
 }
 
 func funReverse(_ context.Context, _ *World, argv []Node) (Node, error) {
@@ -281,7 +261,12 @@ func funReverse(_ context.Context, _ *World, argv []Node) (Node, error) {
 		return nil, err
 	}
 	if _, ok := argv[0].(String); ok {
-		return coerceToString(result)
+		var buffer _StringBuilder
+		err = seqEach(result, func(value Node) error {
+			buffer.Add(value)
+			return nil
+		})
+		return buffer.Sequence(), err
 	}
 	return result, nil
 }
@@ -343,7 +328,7 @@ func funSubSeq(ctx context.Context, w *World, args []Node) (Node, error) {
 			return nil, ErrExpectedNumber
 		}
 	}
-	var buffer _SequenceBuilder
+	var buffer _SeqBuilder
 	if _, ok := args[0].(String); ok {
 		buffer = &_StringBuilder{}
 	} else {
