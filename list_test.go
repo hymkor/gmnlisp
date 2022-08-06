69bc8e4951597dd6ef87d448d24c71b4f436b03b
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-08-06 21:29:29 +0900

    Add test (subst) does not destroy original lists

--- list_test.go
+++ list_test.go
@@ -34,12 +34,24 @@ func TestList(t *testing.T) {
 	assertEqual(t, `(nth 2 '(10 20 30 40))`, Integer(30))
 	assertEqual(t, `(nthcdr 2 '(10 20 30 40))`, List(Integer(30), Integer(40)))
 
+}
+
+func TestSubst(t *testing.T) {
 	assertEqual(t, `
-	(let ((collection (list
-		(cons "X"  1) (cons "Y"  2) (cons "Z" 4))))
-		(subst (cons "X" 7) (assoc "X" collection) collection))`,
+	(let ((m '(("X" . 1) ("Y" . 2) ("Z" . 4))))
+		(subst (cons "X" 7) (assoc "X" m) m))`,
 		List(
 			&Cons{Car: String("X"), Cdr: Integer(7)},
+			&Cons{Car: String("Y"), Cdr: Integer(2)},
+			&Cons{Car: String("Z"), Cdr: Integer(4)}))
+
+	// subst does not destoroy original list
+	assertEqual(t, `
+	(let ((m '(("X" . 1) ("Y" . 2) ("Z" . 4))))
+		(subst (cons "X" 7) (assoc "X" m) m)
+		m)`,
+		List(
+			&Cons{Car: String("X"), Cdr: Integer(1)},
 			&Cons{Car: String("Y"), Cdr: Integer(2)},
 			&Cons{Car: String("Z"), Cdr: Integer(4)}))
 }
