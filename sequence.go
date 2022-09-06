99b34f6b91a3014992ffe4c2346a2bc6d1e217ac
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-09-06 19:20:49 +0900

    Remove (find) (position) and simplify (member)

--- sequence.go
+++ sequence.go
@@ -418,104 +418,6 @@ func funReverse(_ context.Context, _ *World, argv []Node) (Node, error) {
 	return result, nil
 }
 
-func getTestParameter(kwargs map[Keyword]Node) (func(context.Context, *World, Node, Node) (bool, error), error) {
-	if test, ok := kwargs[":test"]; ok {
-		caller, ok := test.(Callable)
-		if !ok {
-			return nil, ErrExpectedFunction
-		}
-		return func(c context.Context, w *World, left, right Node) (bool, error) {
-			result, err := caller.Call(c, w, List(left, right))
-			return HasValue(result), err
-		}, nil
-	} else {
-		return func(_ context.Context, _ *World, left, right Node) (bool, error) {
-			return left.Equals(right, STRICT), nil
-		}, nil
-	}
-}
-
-func funFind(c context.Context, w *World, argv []Node, kwargs map[Keyword]Node) (Node, error) {
-	expr := argv[0]
-	list := argv[1]
-	test, err := getTestParameter(kwargs)
-	if err != nil {
-		return nil, err
-	}
-	var found Node
-
-	err = seqEach(list, func(value Node) error {
-		eq, err := test(c, w, expr, value)
-		if err != nil {
-			return err
-		}
-		if eq {
-			found = value
-			return io.EOF
-		}
-		return nil
-	})
-	if err == io.EOF {
-		return found, nil
-	}
-	return Null, nil
-}
-
-func funMember(c context.Context, w *World, argv []Node, kwargs map[Keyword]Node) (Node, error) {
-	expr := argv[0]
-	list := argv[1]
-	test, err := getTestParameter(kwargs)
-	if err != nil {
-		return nil, err
-	}
-
-	for HasValue(list) {
-		seq, ok := list.(_Sequence)
-		if !ok {
-			return nil, ErrExpectedSequence
-		}
-		value, rest, ok, _ := seq.firstAndRest()
-		if !ok {
-			break
-		}
-		eq, err := test(c, w, expr, value)
-		if err != nil {
-			return nil, err
-		}
-		if eq {
-			return list, nil
-		}
-		list = rest
-	}
-	return Null, nil
-}
-
-func funPosition(c context.Context, w *World, argv []Node, kwargs map[Keyword]Node) (Node, error) {
-	expr := argv[0]
-	list := argv[1]
-	test, err := getTestParameter(kwargs)
-	if err != nil {
-		return nil, err
-	}
-	count := 0
-
-	err = seqEach(list, func(value Node) error {
-		eq, err := test(c, w, expr, value)
-		if err != nil {
-			return err
-		}
-		if eq {
-			return io.EOF
-		}
-		count++
-		return nil
-	})
-	if err == io.EOF {
-		return Integer(count), nil
-	}
-	return Null, nil
-}
-
 func funSubSeq(ctx context.Context, w *World, args []Node) (Node, func(Node) error, error) {
 	if len(args) < 2 {
 		return nil, nil, ErrTooFewArguments
@@ -596,4 +498,25 @@ func funSubSeq(ctx context.Context, w *World, args []Node) (Node, func(Node) err
 			count++
 		}
 	}, ignoreEOF(err)
+}
+
+func funMember(c context.Context, w *World, argv []Node) (Node, error) {
+	expr := argv[0]
+	list := argv[1]
+
+	for HasValue(list) {
+		seq, ok := list.(Sequence)
+		if !ok {
+			return nil, ErrExpectedSequence
+		}
+		value, rest, ok, _ := seq.FirstAndRest()
+		if !ok {
+			break
+		}
+		if expr.Equals(value, STRICT) {
+			return list, nil
+		}
+		list = rest
+	}
+	return Null, nil
 }
--- sequence_test.go
+++ sequence_test.go
@@ -100,27 +100,6 @@ func TestReverse(t *testing.T) {
 	assertEqual(t, `(reverse "12345")`, String("54321"))
 }
 
-func TestFind(t *testing.T) {
-	assertEqual(t, `(find 2 '(1 2 3))`, Integer(2))
-	assertEqual(t, `(find 2.0 '(1 2 3))`, Null)
-	assertEqual(t, `(find 2.0 '(1 2 3) :test #'(lambda (a b) (equalp a b)))`, Integer(2))
-	assertEqual(t, `(find 2.0 '(1 2 3) :test #'(lambda (a b) (eql a b)))`, Null)
-}
-
-func TestMember(t *testing.T) {
-	assertEqual(t, `(member 'c '(a b c d e))`,
-		List(NewSymbol("c"), NewSymbol("d"), NewSymbol("e")))
-	assertEqual(t, `(member #\c "abcd")`, String("cd"))
-	assertEqual(t, `(member #\C "abcd" :test #'(lambda (a b) (equalp a b)))`, String("cd"))
-}
-
-func TestPosition(t *testing.T) {
-	assertEqual(t, `(position 'c '(a b c d e))`, Integer(2))
-	assertEqual(t, `(position #\c "abcd")`, Integer(2))
-	assertEqual(t, `(position #\C "abcd")`, Null)
-	assertEqual(t, `(position #\C "abcd" :test #'(lambda (a b) (equalp a b)))`, Integer(2))
-}
-
 func TestSubSeq(t *testing.T) {
 	assertEqual(t, `(subseq "12345" 2 4)`, String("34"))
 	assertEqual(t, `(subseq "12345" 2)`, String("345"))
