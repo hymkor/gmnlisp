32b0e1e9baf7a3d01616f207cd18e2f15fae83c4
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-10-31 23:57:15 +0900

    Remove unused function: matchError

--- progn.go
+++ progn.go
@@ -362,24 +362,6 @@ func (e *ErrorNode) Equals(n Node, m EqlMode) bool {
 	return errors.Is(e.Value, f.Value) || errors.Is(f.Value, e.Value)
 }
 
-func matchError(ctx context.Context, w *World, casedSymbol Node, happenError error) (bool, error) {
-	if happenError == nil {
-		return false, nil
-	}
-	if casedSymbol == NewSymbol("error") {
-		return true, nil
-	}
-	casedNode, err := casedSymbol.Eval(ctx, w)
-	if err != nil {
-		return false, err
-	}
-	errNode, ok := casedNode.(*ErrorNode)
-	if !ok {
-		return false, fmt.Errorf("not an error object in hander-case: %s", ToString(casedSymbol, PRINT))
-	}
-	return errors.Is(happenError, errNode.Value), nil
-}
-
 func cmdWithHandler(ctx context.Context, w *World, list Node) (Node, error) {
 	// ISLisp
 	handlerNode, list, err := w.ShiftAndEvalCar(ctx, list)
