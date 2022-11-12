312667f215ec5a57d486095babd71427269f369e
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-11-13 01:42:23 +0900

    pkg/common: remove (when) (unless)

--- pkg/common/main.go
+++ pkg/common/main.go
@@ -15,27 +15,24 @@ func init() {
 }
 
 var Functions = Variables{
-	NewSymbol("member"):       &KWFunction{C: 2, F: funMember},
-	NewSymbol("find"):         &KWFunction{C: 2, F: funFind},
-	NewSymbol("position"):     &KWFunction{C: 2, F: funPosition},
-	NewSymbol("defparameter"): SpecialF(cmdDefparameter),
-	NewSymbol("defvar"):       SpecialF(cmdDefvar),
-	NewSymbol("prin1"):        &Function{C: 1, F: funPrin1},
-	NewSymbol("princ"):        &Function{C: 1, F: funPrinc},
-	NewSymbol("print"):        &Function{C: 1, F: funPrint},
-	NewSymbol("terpri"):       &Function{C: -1, F: funTerpri},
-	NewSymbol("write"):        &KWFunction{C: 1, F: funWrite},
-	NewSymbol("write-line"):   SpecialF(cmdWriteLine),
-	NewSymbol("when"):         SpecialF(cmdWhen),
-	NewSymbol("unless"):       SpecialF(cmdUnless),
-	NewSymbol("nth"):          &Function{C: 2, F: funNth},
-	NewSymbol("nthcdr"):       &Function{C: 2, F: funNthcdr},
-	NewSymbol("cadddr"):       &Function{C: 1, F: funCadddr},
-	NewSymbol("caddr"):        &Function{C: 1, F: funCaddr},
-	NewSymbol("cadr"):         &Function{C: 1, F: funCadr},
-	NewSymbol("cdddr"):        &Function{C: 1, F: funCdddr},
-	NewSymbol("cddr"):         &Function{C: 1, F: funCddr},
-	//NewSymbol("first"):                       &Function{C: 1, F: funGetCar},
+	NewSymbol("member"):         &KWFunction{C: 2, F: funMember},
+	NewSymbol("find"):           &KWFunction{C: 2, F: funFind},
+	NewSymbol("position"):       &KWFunction{C: 2, F: funPosition},
+	NewSymbol("defparameter"):   SpecialF(cmdDefparameter),
+	NewSymbol("defvar"):         SpecialF(cmdDefvar),
+	NewSymbol("prin1"):          &Function{C: 1, F: funPrin1},
+	NewSymbol("princ"):          &Function{C: 1, F: funPrinc},
+	NewSymbol("print"):          &Function{C: 1, F: funPrint},
+	NewSymbol("terpri"):         &Function{C: -1, F: funTerpri},
+	NewSymbol("write"):          &KWFunction{C: 1, F: funWrite},
+	NewSymbol("write-line"):     SpecialF(cmdWriteLine),
+	NewSymbol("nth"):            &Function{C: 2, F: funNth},
+	NewSymbol("nthcdr"):         &Function{C: 2, F: funNthcdr},
+	NewSymbol("cadddr"):         &Function{C: 1, F: funCadddr},
+	NewSymbol("caddr"):          &Function{C: 1, F: funCaddr},
+	NewSymbol("cadr"):           &Function{C: 1, F: funCadr},
+	NewSymbol("cdddr"):          &Function{C: 1, F: funCdddr},
+	NewSymbol("cddr"):           &Function{C: 1, F: funCddr},
 	NewSymbol("second"):         &Function{C: 1, F: funCadr},
 	NewSymbol("third"):          &Function{C: 1, F: funCaddr},
 	NewSymbol("concatenate"):    defConcatenate,
@@ -84,28 +81,6 @@ func cmdDefvar(ctx context.Context, w *World, list Node) (Node, error) {
 		return value
 	})
 	return symbol, err
-}
-
-func cmdWhen(ctx context.Context, w *World, args Node) (Node, error) {
-	cond, args, err := w.ShiftAndEvalCar(ctx, args)
-	if err != nil {
-		return nil, err
-	}
-	if IsNull(cond) {
-		return Null, nil
-	}
-	return Progn(ctx, w, args)
-}
-
-func cmdUnless(ctx context.Context, w *World, args Node) (Node, error) {
-	cond, args, err := w.ShiftAndEvalCar(ctx, args)
-	if err != nil {
-		return nil, err
-	}
-	if HasValue(cond) {
-		return Null, nil
-	}
-	return Progn(ctx, w, args)
 }
 
 type KWFunction struct {
