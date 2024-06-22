3f2c23a5149c57503435ac072b373b8dbb15745c
HAYAMA_Kaoru <iyahaya@nifty.com>
2024-06-22 15:38:20 +0900

    Dirty hack for the problem `(with-handler)` did not work for system-error

--- cond.go
+++ cond.go
@@ -20,7 +20,29 @@ func cmdWithHandler(ctx context.Context, w *World, node Node) (Node, error) {
 	D := w.NewDynamics()
 	defer D.Close()
 	D.Set(activeHandleFuncKey, handler)
-	return Progn(ctx, w, node)
+	value, err := Progn(ctx, w, node)
+
+	// 本来、場合によっては継続処理があるため、ハンドラー関数はエラーが起きた場所で
+	// 呼ばなければいけないが、 古いコードは error を返して、error の処理元で
+	// ハンドラー関数を呼ぶことを想定し、エラー発生箇所で呼べていない
+	//
+	// エラー発生箇所でハンドラー関数が呼ばれて、ここに戻る時は大域脱出系エラーで
+	// 戻ってくるはずなので、そういった場合は、ここでハンドラー関数を呼ばない
+	// ようにしている
+	// (将来的にはエラーが起きた場所で呼ぶよう変更しなくてはいけない)
+
+	var e1 *ErrEarlyReturns // block & return-from
+	var e2 *ErrThrown       // catch & throw
+	var e3 *_TagBody        // tagbody & go
+	if err == nil || errors.As(err, &e1) || errors.As(err, &e2) || errors.As(err, &e3) {
+		return value, err
+	}
+
+	_, err2 := handler.Call(ctx, w, &Cons{Car: &ErrorNode{Value: err}, Cdr: Null})
+	if err2 != nil {
+		return nil, err2
+	}
+	return nil, err
 }
 
 type ErrContinueCondition struct {
