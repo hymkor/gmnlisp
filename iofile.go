4bcd9ab8df74d450f984aaaad75683370f74adab
HAYAMA_Kaoru <iyahaya@nifty.com>
2024-12-19 13:44:51 +0900

    Rewrite `*IOFile`

--- iofile.go
+++ iofile.go
@@ -8,14 +8,42 @@ import (
 )
 
 type IOFile struct {
-	inputStream
-	writer *bufio.Writer
-	column int
+	reader   *bufio.Reader
+	writer   *bufio.Writer
+	file     *os.File
+	isClosed bool
+	column   int
+}
+
+var _ _Reader = &IOFile{}
+
+func (iof *IOFile) Read(b []byte) (int, error) {
+	iof.writer.Flush()
+	return iof.reader.Read(b)
+}
+
+func (iof *IOFile) ReadByte() (byte, error) {
+	iof.writer.Flush()
+	return iof.reader.ReadByte()
+}
+
+func (iof *IOFile) ReadRune() (rune, int, error) {
+	iof.writer.Flush()
+	return iof.reader.ReadRune()
+}
+
+func (iof *IOFile) UnreadRune() error {
+	return iof.reader.UnreadRune()
 }
 
 func (iof *IOFile) Close() error {
 	iof.writer.Flush()
-	return iof.inputStream.Close()
+	iof.isClosed = true
+	return iof.file.Close()
+}
+
+func (iof *IOFile) IsClosed() bool {
+	return iof.isClosed
 }
 
 func (iof *IOFile) Column() int {
@@ -44,17 +72,42 @@ func (t *IOFile) String() string {
 	return "(*inputOutputStream)"
 }
 
+func (i *IOFile) FilePosition() (int64, error) {
+	i.writer.Flush()
+	z, err := i.file.Seek(0, os.SEEK_CUR)
+	if err != nil {
+		return 0, err
+	}
+	return z - int64(i.reader.Buffered()), nil
+}
+
+func (i *IOFile) SetFilePosition(n int64) (int64, error) {
+	i.writer.Flush()
+	ret, err := i.file.Seek(n, os.SEEK_SET)
+	i.reader.Reset(i.file)
+	return ret, err
+}
+
+func (i *IOFile) QueryStreamReady() (Node, error) {
+	if i.isClosed {
+		return Null, StreamError{Stream: i}
+	}
+	i.writer.Flush()
+	if _, err := i.file.Seek(0, os.SEEK_CUR); err != nil {
+		return Null, nil
+	}
+	return True, nil
+}
+
 func newIoFile(fname string) (*IOFile, error) {
 	f, err := os.OpenFile(fname, os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
 		return nil, err
 	}
 	return &IOFile{
-		inputStream: inputStream{
-			_Reader: bufio.NewReader(f),
-			file:    f,
-		},
+		reader: bufio.NewReader(f),
 		writer: bufio.NewWriter(f),
+		file:   f,
 	}, nil
 }
 
