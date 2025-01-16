da2faf97c6fce356598196ee98ee7329b057da6e
HAYAMA_Kaoru <iyahaya@nifty.com>
2025-01-16 20:54:01 +0900

    Fix: the column counting on `(*_WriterNode) Write` and `(_OutputFileStream) Write`

--- writer.go
+++ writer.go
@@ -16,16 +16,16 @@ func (w *_WriterNode) Column() int {
 	return w.column
 }
 
-func (w *_WriterNode) Write(p []byte) (nn int, err error) {
-	nn, err = w._Writer.Write(p)
-	if nn >= 1 {
-		if pos := bytes.LastIndexByte(p[:nn], '\n'); pos >= 0 {
-			w.column = nn - pos
+func (w *_WriterNode) Write(p []byte) (int, error) {
+	n, err := w._Writer.Write(p)
+	if n >= 1 {
+		if pos := bytes.LastIndexByte(p[:n], '\n'); pos >= 0 {
+			w.column = n - pos - 1
 		} else {
-			w.column += nn
+			w.column += n
 		}
 	}
-	return nn, err
+	return n, err
 }
 
 type _OutputFileStream struct {
@@ -46,11 +46,11 @@ func (o *_OutputFileStream) Column() int {
 	return o.column
 }
 
-func (o *_OutputFileStream) Write(p []byte) (nn int, err error) {
+func (o *_OutputFileStream) Write(p []byte) (int, error) {
 	n, err := o.w.Write(p)
 	if n >= 1 {
 		if pos := bytes.LastIndexByte(p[:n], '\n'); pos >= 0 {
-			o.column = n - pos
+			o.column = n - pos - 1
 		} else {
 			o.column += n
 		}
