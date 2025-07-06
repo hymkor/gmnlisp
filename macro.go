35fb694bf1930a5c9a6ac7d79f62e2caa99dc871
HAYAMA Kaoru <iyahaya@nifty.com>
2025-07-06 17:05:51 +0900

    Move the the method of `_Macro` in `newtypes.go` to `macro.go`

--- macro.go
+++ macro.go
@@ -23,6 +23,14 @@ func (*_Macro) ClassOf() Class {
 	return macroClass
 }
 
+func (t *_Macro) Equals(Node, EqlMode) bool {
+	return false
+}
+
+func (t *_Macro) String() string {
+	return fmt.Sprintf("<macro>: %p", t)
+}
+
 type _JoinedForm []Node
 
 var joinedFormClass = registerNewBuiltInClass[_JoinedForm]("<joined-form>")
